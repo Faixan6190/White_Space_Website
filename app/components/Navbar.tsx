1a120fe72f5de76728fe021fcaa90b8d5e518779
Faixan6190 <faixan6190@gmail.com>
2024-11-14 22:55:03 +0500

    navbar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,28 +1,55 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import logo from "@/app/assets/Logo.png";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <div className="w-full h-[92px] py-4 px-[220px] bg-[#043873] flex justify-between items-center">
-        <div className="w-[191px] h-[34px]">
-          <Image src={logo} alt="" />
-        </div>
-        <div className="w-[737.5px] h-[60px] flex justify-between items-center">
-          <div className="w-[549px] h-[23px] ">
-            <ul className="flex flex-row justify-between text-white">
-              <li>Products</li>
-              <li>Solutions</li>
-              <li>Resources</li>
-              <li>Pricing</li>
-            </ul>
-          </div>
-          <div className="w-[126px] h-[60px]">
-            <button className="px-10 py-4 rounded-lg bg-[#FFE492]">Login</button>
-          </div>
-        </div>
+    <nav className="w-full h-[92px] py-4 px-6 md:px-[220px] bg-[#043873] flex justify-between items-center">
+      {/* Logo */}
+      <div className="w-[191px] h-[34px]">
+        <Image src={logo} alt="Logo" />
       </div>
-    </>
+
+      {/* Desktop Menu */}
+      <div className="hidden md:flex w-[737.5px] h-[60px] justify-between items-center">
+        <ul className="flex space-x-8 text-white">
+          <li>Products</li>
+          <li>Solutions</li>
+          <li>Resources</li>
+          <li>Pricing</li>
+        </ul>
+        <button className="px-10 py-4 rounded-lg bg-[#FFE492]">Login</button>
+      </div>
+
+      {/* Mobile Menu Toggle */}
+      <div className="md:hidden">
+        <button onClick={toggleMenu} className="text-white focus:outline-none">
+          {isOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
+        </button>
+      </div>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden absolute top-[92px] left-0 w-full bg-[#043873] text-white flex flex-col items-center space-y-4 py-6">
+          <ul className="flex flex-col space-y-4">
+            <li>Products</li>
+            <li>Solutions</li>
+            <li>Resources</li>
+            <li>Pricing</li>
+          </ul>
+          <button className="px-10 py-4 rounded-lg bg-[#FFE492]">Login</button>
+        </div>
+      )}
+    </nav>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <Main />
       <UseAsExtension />
-      <CustomiseItToYourNeed />
+      <CustomiseItToYourNeed /> */}
     </div>
   );
 }
